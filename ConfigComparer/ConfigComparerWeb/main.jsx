a89c5749895dcf27e9cd848e4f77680f49ed5a9b
Sergey Rubtsov <rsv.astana@gmail.com>
2016-03-25 10:44:53 +0600

    отказ от reactify, использование babel

--- ConfigComparer/ConfigComparerWeb/main.jsx
+++ ConfigComparer/ConfigComparerWeb/main.jsx
@@ -2,9 +2,32 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Hello = React.createClass({
-  render: function() {
-    return <h1>Hello, World!</h1>;
-  }
+    getInitialState: function () {
+        return {text: undefined}
+    },
+    
+    getDefaultProps: function () {
+        return {text: "World"}
+    },
+    
+    onTextChange: function (e) {
+        e.preventDefault();
+        this.setState({text: e.target.value});
+    },
+    
+    render: function() {
+        var text;
+        if (this.state.text) {
+            text = this.state.text;
+        } else {
+            text = this.props.text;
+        }
+        return (
+            <div>
+                <input type="text" value={this.state.text} onChange={this.onTextChange} />
+                <h1>Hello, {text}!</h1>
+            </div>);
+    }
 });
 
 ReactDOM.render(<Hello />, document.getElementById('container'));
